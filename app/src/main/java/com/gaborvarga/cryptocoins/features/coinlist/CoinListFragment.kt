5cd40752e7a6691adebb4b354a2e8663b28c77a0
Anonymus <anonymus@aldi.com>
2024-03-03 16:19:19 +0100

    feat: add skeleton CoinListFragment and its layout

---
+++ app/src/main/java/com/gaborvarga/cryptocoins/features/coinlist/CoinListFragment.kt
@@ -0,0 +1,6 @@
+package com.aldi.cryptocoins.features.coinlist
+
+import androidx.fragment.app.Fragment
+import com.aldi.cryptocoins.R
+
+class CoinListFragment : Fragment(R.layout.fragment_coinlist)
